2e3e8c7df9e5c04ee1adfb0822d32463fd68e885
DANTEE-DOT <tsidkenu-temisan.daniel@binghamuni.edu.ng>
2024-12-30 14:01:18 -0700

    Flora commit

--- Kacana.py
+++ Kacana.py
@@ -43,8 +43,6 @@ def open_new_window():
     "japan": "nihon",
     "cat": "neko",
     "dog": "inu",
-    "lion":'shishido',
-    "tiger":'tora'
 }
 
     def search(word):
@@ -66,11 +64,54 @@ def open_new_window():
 
 button = Button(window, text="Japanese", width=50, height=2,font=('Times New Roman', 14), command=open_new_window)
 button.pack(pady=20)
-wuiehwiuevhwvijwhv
 
-button2= Button(window, text="Igala", width=50, height=2,font=('Times New Roman', 14))
+button2= Button(window, text="Igala", width=50, height=2,font=('Times New Roman', 14), )
 button2.pack(pady=20)
-button3= Button(window, text="Japanese", width=50, height=2,font=('Times New Roman', 14))
+def open_new_window():
+    new_window = Toplevel(window)
+    new_window.title("French Window")
+    new_window.geometry("300x200")
+
+    entry_text = Entry(new_window)
+    entry_text.pack()
+
+    result = StringVar()
+    result_label = Label(new_window, textvariable=result)
+    result_label.pack()
+
+    French_dictionary = {
+    "hello": "konnichiwa",
+    "thank you": "arigatou",
+    "goodbye": "sayounara",
+    "good morning": "ohayou",
+    "good evening": "konbanwa",
+    "excuse me": "sumimasen",
+    "yes": "hai",
+    "no": "iie",
+    "love": "ai",
+    "friend": "tomodachi",
+    "family": "kazoku",
+    "food": "tabemono",
+    "drink": "nomimono",
+    "school": "gakkou",
+    "teacher": "sensei",
+    "student": "gakusei",
+    "time": "jikan",
+    "japan": "nihon",
+    "cat": "neko",
+    "dog": "inu",
+}
+
+    def search(word):
+        if word in French_dictionary.keys():
+            result.set(French_dictionary[word])
+            print(French_dictionary[word])
+        else:
+            result.set("Not Found")
+
+    search_btn = Button(new_window, text="Search", font=("Times New Roman", 14), command=lambda: search(entry_text.get()))
+    search_btn.pack()
+button3= Button(window, text="French", width=50, height=2,font=('Times New Roman', 14), command=open_new_window)
 button3.pack(pady=20)
 button4= Button(window, text="Igbo", width=50, height=2,font=('Times New Roman', 14))
 button4.pack(pady=20)
